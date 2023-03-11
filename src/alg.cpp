557b2c4538524550cb358bca7c2a639c4be443d1
Kirill Kashkirov <124998794+Kashkirov@users.noreply.github.com>
2023-03-11 15:36:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,21 @@
 // Copyright 2021 NNTU-CS
-
+int ApproriateTarget(int* arr, int target, int ID) {
+    if (arr[ID] == target) {
+        return 1;
+    }
+    return 0;
+}
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int count2 = 0;
+    int middleCrossing = size / 2;
+    int i = 0;
+    if (size % 2 != 0 ) {
+        count2 += ApproriateTarget(arr, value, size - 1);
+    }
+    while (i < middleCrossing) {
+        count2 += ApproriateTarget(arr, value, i);
+        count2 += ApproriateTarget(arr, value, i + middleCrossing);
+        ++i;
+    }
+    return count2;
 }
