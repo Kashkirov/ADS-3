3acebfb6d6c5e952bb620c3138f29d50bcc32eb8
Kirill Kashkirov <124998794+Kashkirov@users.noreply.github.com>
2023-03-11 18:24:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -9,7 +9,7 @@ int cbinsearch(int *arr, int size, int value) {
     int count2 = 0;
     int middleCrossing = size / 2;
     int i = 0;
-    if (size % 2 != 0 ) {
+    if (size % 2 != 0) {
         count2 += ApproriateTarget(arr, value, size - 1);
     }
     while (i < middleCrossing) {
